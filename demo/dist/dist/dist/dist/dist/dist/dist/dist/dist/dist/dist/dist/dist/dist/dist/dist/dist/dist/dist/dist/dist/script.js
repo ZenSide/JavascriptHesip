f3a8dea344a4771d5b37f922198529fc7ef14767
Nicolas Janel <janel.nicolas@zenside.fr>
2022-04-12 11:13:02 +0400

    no babel

--- demo/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/script.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-fetch("https://www.boredapi.com/api/activity").then(function (r) {
-  console.log(r.json());
-  console.log(JSON.parse(r.json()));
-});
-//# sourceMappingURL=script.js.map
